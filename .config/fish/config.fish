e615ae4a2676a64218dedfc07a0af703f13a22df
Seokmin Hong (Ray) <ghdtjrald240@gmail.com>
2022-06-28 16:46:31 +0900

    Comment out zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,6 @@ abbr --add --global -- cat 'bat'
 abbr --add --global -- ls 'exa'
 abbr --add --global -- ll 'exa -lha'
 abbr --add --global -- tree 'exa --tree -L 2 -alh'
-abbr --add --global -- cd 'z'
+# abbr --add --global -- cd 'z'
 
-zoxide init fish | source
+# zoxide init fish | source
