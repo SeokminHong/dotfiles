bbc53d3cfa68fe448790f50466b56c62d7b810d3
Seokmin Hong <ghdtjrald240@gmail.com>
2022-06-28 18:08:51 +0900

    Set theme configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Theme (bobthefish)
+set -g theme_nerd_fonts yes
+set -g theme_display_date no
+set -g theme_show_exit_status yes
+set -g theme_display_cmd_duration yes
+
 abbr --add --global -- cat 'bat'
 abbr --add --global -- ls 'exa'
 abbr --add --global -- ll 'exa -lha'
