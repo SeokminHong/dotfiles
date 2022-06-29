9bb93bb171768d0b910025be7abed0321f4a2609
Seokmin Hong <ghdtjrald240@gmail.com>
2022-06-29 14:44:01 +0900

    Move store to root

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,20 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set architecture (uname -m | tr '[:upper:]' '[:lower:]')
-switch $architecture
-    case 'amd64'; set architecture "x86_64"
-    case 'arm64'; set architecture "aarch64"
-end
-
-set platform (uname -s | tr '[:upper:]' '[:lower:]')
-switch $platform
-    case 'linux'; set platform "unknown-linux-gnu"
-    case 'darwin'; set platform "apple-darwin"
-end
-
 # Path
-fish_add_path ~/.config/fish/store/starship
+fish_add_path ~/bin
 
 # Abbreviations
 abbr --add --global -- cat 'bat'
@@ -24,5 +12,5 @@ abbr --add --global -- ll 'exa -lha'
 abbr --add --global -- tree 'exa --tree -L 2 -alh'
 # abbr --add --global -- cd 'z'
 
-starship-$architecture-$platform init fish | source
+starship init fish | source
 # zoxide init fish | source
--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 mkdir -p ~/.config
 cp -r .config/fish ~/.config
 cp .iex.exs ~/
+
+mkdir -p ~/bin
+
+ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+switch $ARCH
+    case 'amd64'; ARCH="x86_64"
+    case 'arm64'; ARCH="aarch64"
+end
+
+PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
+switch $PLATFORM
+    case 'linux'; PLATFORM="unknown-linux-gnu"
+    case 'darwin'; PLATFORM="apple-darwin"
+end
+
+cp "store/starship/starship-$ARCH-$PLATFORM" ~/bin/starship
